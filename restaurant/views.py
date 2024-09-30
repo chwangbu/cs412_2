fffa0d0d28f2e92c7394c14dca6be7f0feef31b2
chwangbu <bryho@bu.edu>
2024-09-30 19:50:48 -0400

    Fixed poor handling of requests: original was redirecting without processing form data. +Flawed redirection logic: Was redirecting from order.view without processing therefore confirmation view can't register POST request

--- restaurant/views.py
+++ restaurant/views.py
@@ -18,16 +18,6 @@ def order(request):
     daily_special = random.choice(list(menu_items.keys()))
 
     if request.method == 'POST':
-        return redirect('confirmation')
-
-    context = {
-        'daily_special': daily_special,
-        'menu_items': menu_items,
-    }
-    return render(request, 'restaurant/order.html', context)
-
-def confirmation(request):
-    if request.method == "POST":
         ordered_items = {item: menu_items[item] for item in request.POST if item in menu_items}
         special_instructions = request.POST.get('instructions', '')
         name = request.POST.get('name', '')
@@ -47,8 +37,16 @@ def confirmation(request):
             'ready_time': ready_time.strftime("%Y-%m-%d %H:%M"),
         }
         return render(request, 'restaurant/confirmation.html', context)
-    else:
-        return render(request, 'restaurant/order.html')
+    
+    context = {
+        'daily_special': daily_special,
+        'menu_items': menu_items,
+    }
+        
+    return render(request, 'restaurant/order.html', context)
+
+def confirmation(request): 
+    return render(request, 'restaurant/confirmation.html')
     
 def landing_page(request):
     return render(request, 'restaurant/main.html')

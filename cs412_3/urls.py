de14614cdadf0d51b7756ed306b5f9a9d26a5e87
chwangbu <bryho@bu.edu>
2024-09-30 17:04:42 -0400

    Issues with loading - Fixes made: Mapped the root URL + Django could not find my file so changed template directories so that Django could fine it

--- cs412_3/urls.py
+++ cs412_3/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from restaurant.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #adding restaurant URL
     path('restaurant/', include('restaurant.urls')),
+    #root URL
+    path('', main, name='home'),
 ]
